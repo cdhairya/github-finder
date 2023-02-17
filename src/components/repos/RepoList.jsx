69f12cb595a95de91f87aef3abe3c78b2e159145
Dhairya Chheda <dhairyakchheda@gmail.com>
2023-02-17 19:38:16 +0530

    Repo List component

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -2,14 +2,14 @@ import PropTypes from "prop-types";
 
 function RepoList({ repos }) {
   return (
-    <div rounded-lg shadow-lg card bg-base-100>
+    <div className="rounded-lg shadow-lg card bg-base-100">
       <div className="card-body">
         <h2 className="text-3xl my-4 card-title font-bold">
           Latest Repositories
         </h2>
 
         {repos.map((repo) => (
-          <h3>{repo.name}</h3>
+          <h3 key={repo.id}>{repo.name}</h3>
         ))}
       </div>
     </div>
